05a9a8a9f7822f5c3c2e89a3b8af5d5af46d4020
IronTopi <tobias.aretz@rwth-aachen.de>
2023-03-08 23:00:21 +0000

    auth test

--- backend/src/test/auth_test.py
+++ backend/src/test/auth_test.py
@@ -5,17 +5,19 @@ from fastapi import HTTPException
 from fastapi.security.http import HTTPBasicCredentials
 from auth import validateCredentials
 
-def test_LoginSuccess ():
+
+def test_LoginSuccess():
     storedUsername = os.environ["API_USER"].encode("utf-8")
     storedPassword = os.environ["API_PASSWORD"].encode("utf-8")
-    validCredentials = HTTPBasicCredentials (username=storedUsername, password=storedPassword)
+    validCredentials = HTTPBasicCredentials(username=storedUsername, password=storedPassword)
 
-    assert validateCredentials (validCredentials)
+    assert validateCredentials(validCredentials)
 
-def test_LoginFailure ():
+
+def test_LoginFailure():
     storedUsername = (os.environ["API_USER"] + "nope").encode("utf-8")
     storedPassword = (os.environ["API_PASSWORD"] + "nope").encode("utf-8")
-    validCredentials = HTTPBasicCredentials (username=storedUsername, password=storedPassword)
+    validCredentials = HTTPBasicCredentials(username=storedUsername, password=storedPassword)
 
     with pytest.raises(HTTPException) as e_info:
-        validateCredentials (validCredentials)
+        validateCredentials(validCredentials)
